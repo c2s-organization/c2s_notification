7089e46c8f215d08f33c12431b752eddc8f9c0ed
Marco Castro <marcodotcastro@gmail.com>
2024-10-18 11:35:02 -0300

    build: docker

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Criar o banco de dados, se não existir
+bundle exec rails db:create
+
+# Executar as migrações
+bundle exec rails db:migrate
+
+# Remover PID antigo do servidor (se necessário)
+rm -f /app/tmp/pids/server.pid
+
+# Iniciar o servidor Rails
+exec bundle exec rails s -b '0.0.0.0'
